1979109e5b5bde987c2c5a55ef50cf6c16eaf421
ctacke <ctacke@gmail.com>
2022-07-07 20:55:24 -0500

    Thermo13 driver

---
+++ Source/Thermo13/Driver/Thermo13.cs
@@ -0,0 +1,19 @@
+﻿using Meadow.Hardware;
+
+namespace Meadow.Foundation.mikroBUS
+{
+    /// <summary>
+    /// Represents a mikroBUS Thermo 13 Click board
+    /// </summary>
+    public class Thermo13 : Bh1900Nux
+    {
+        /// <summary>
+        /// Creates a Thermo13 driver
+        /// </summary>
+        /// <param name="i2cBus"></param>
+        public Thermo13(II2cBus i2cBus)
+            : base(i2cBus, Address.Default)
+        {
+        }
+    }
+}
