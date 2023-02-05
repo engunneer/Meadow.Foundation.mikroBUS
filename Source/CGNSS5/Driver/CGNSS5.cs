f2322dadf57f2266710cc5a4199011cc60cb34b6
Adrian Stevens <adrian.stevens@gmail.com>
2023-02-04 22:06:06 -0800

    Add GNNS5 driver

---
+++ Source/CGNSS5/Driver/CGNSS5.cs
@@ -0,0 +1,18 @@
+﻿using Meadow.Foundation.Sensors.Gnss;
+using Meadow.Hardware;
+
+namespace Meadow.Foundation.mikroBUS.Sensors.Gnss
+{
+    /// <summary>
+    /// Represents a mikroBUS GNSS 5 board (Neo M8) 
+    /// </summary>
+    public class CGNSS5 : NeoM8
+    {
+        /// <summary>
+        /// Creates a new CGNSS5 object
+        /// </summary>
+        public CGNSS5(IMeadowDevice device, SerialPortName serialPortName, IPin resetPin, IPin ppsPin = null)
+            : base(device, serialPortName, resetPin, ppsPin)
+        { }
+    }
+}
