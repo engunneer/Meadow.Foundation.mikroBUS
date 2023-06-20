540bdbca12dd591e1d339edf951649f19936cf38
Adrian Stevens <adrian.stevens@gmail.com>
2023-06-19 21:36:46 -0700

    Sample cleanup

--- Source/CLEM/Driver/CLEM.cs
+++ Source/CLEM/Driver/CLEM.cs
@@ -1,80 +1,30 @@
-﻿using Meadow.Foundation.Leds;
-using Meadow.Foundation.Sensors.Buttons;
-using Meadow.Hardware;
-using System;
+﻿using Meadow.Hardware;
 
 namespace Meadow.Foundation.mikroBUS.Sensors.Buttons
 {
     /// <summary>
-    /// Represents a mikroBUS Button G,R,Y Click board
+    /// Represents a mikroBUS AC current sensing LEM Click board
     /// </summary>
-    public class CButton : PushButton
+    public class CLEM
     {
-        readonly PwmLed pwmLed;
-
         /// <summary>
-        /// Gets or sets a value indicating whether the LED is on.
+        /// Creates a new CLEM object
         /// </summary>
-        /// <value><c>true</c> if is on; otherwise, <c>false</c>.</value>
-        public bool IsOn
+        /// <param name="spiBus">The SPI bus</param>
+        /// <param name="chipSelectPin">Chip select pin</param>
+        public CLEM(ISpiBus spiBus, IPin chipSelectPin)
         {
-            get => pwmLed.IsOn;
-            set => pwmLed.IsOn = value;
+
         }
 
         /// <summary>
-        /// Gets the brightness of the LED, controlled by a PWM signal
+        /// Creates a new CLEM object
         /// </summary>
-        public float Brightness
+        /// <param name="spiBus">The SPI bus</param>
+        /// <param name="chipSelectPort">Chip select port</param>
+        public CLEM(ISpiBus spiBus, IDigitalInputPort chipSelectPort)
         {
-            get => pwmLed.Brightness;
-            set => pwmLed.SetBrightness(value);
-        }
 
-        /// <summary>
-        /// Creates a new CButton object
-        /// </summary>
-        /// <param name="ledPin">Led pin</param>
-        /// <param name="buttonPin">Button pin</param>
-        public CButton(IPin ledPin, IPin buttonPin)
-            : base(buttonPin, ResistorMode.InternalPullUp)
-        {
-            pwmLed = new PwmLed(ledPin, new Units.Voltage(TypicalForwardVoltage.Green));
-        }
-
-        /// <summary>
-        /// Creates a new CButton object
-        /// </summary>
-        /// <param name="ledPwmPort">Led PWM port</param>
-        /// <param name="buttonInterruptPort">Button interrupt port</param>
-        public CButton(IPwmPort ledPwmPort, IDigitalInputPort buttonInterruptPort)
-            : base(buttonInterruptPort)
-        {
-            pwmLed = new PwmLed(ledPwmPort, new Units.Voltage(TypicalForwardVoltage.Green));
-        }
-
-        /// <summary>
-        /// Start the Blink animation which sets the brightness of the LED alternating between a low and high brightness setting, using the durations provided.
-        /// </summary>
-        public void StartBlink(TimeSpan onDuration, TimeSpan offDuration, float highBrightness = 1f, float lowBrightness = 0f)
-        {
-            pwmLed?.StartBlink(onDuration, offDuration, highBrightness, lowBrightness);
-        }
-
-        /// <summary>
-        /// Start the Pulse animation which gradually alternates the brightness of the LED between a low and high brightness setting, using the durations provided.
-        /// </summary>
-        public void StartPulse(TimeSpan pulseDuration, float highBrightness, float lowBrightness = 0.15F)
-        {
-            pwmLed?.StartPulse(pulseDuration, highBrightness, lowBrightness);
-        }
-
-        /// <summary>
-        /// Stops any running animations
-        /// </summary>
-        public void StopAnimation()
-        {
-            pwmLed?.StopAnimation();
         }
     }
 }
--- Source/CLEM/Sample/CLEM_Sample/MeadowApp.cs
+++ Source/CLEM/Sample/CLEM_Sample/MeadowApp.cs
@@ -3,27 +3,18 @@ using Meadow.Devices;
 using Meadow.Foundation.mikroBUS.Sensors.Buttons;
 using System;
 
-namespace CButton_Sample
+namespace CLEM_Sample
 {
     // Change F7FeatherV2 to F7FeatherV1 for V1.x boards
     public class MeadowApp : App<F7FeatherV2>
     {
         //<!=SNIP=>
 
-        CButton ledButton;
+        CLEM currenClick;
 
         public MeadowApp()
         {
             Console.WriteLine("Initializing ...");
-
-            ledButton = new CButton(Device.Pins.D03, Device.Pins.D04);
-
-            ledButton.StartPulse(TimeSpan.FromSeconds(2), 0.75f, 0);
-            ledButton.Clicked += (s, e) =>
-            {
-                Console.WriteLine("Button clicked");
-                ledButton.IsOn = !ledButton.IsOn;
-            };
         }
 
         //<!=SNOP=>
