30cfbdcce1e65923cebc3251d0cbf4a84f5abb27
Chris Tacke <ctacke@opennetcf.com>
2023-09-25 13:48:21 -0500

    C420T and C420R

---
+++ Source/C420R/Sample/C420R_Sample/MeadowApp.cs
@@ -0,0 +1,45 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.mikroBUS.Sensors;
+using System;
+using System.Threading.Tasks;
+
+namespace CACCurrent_Sample
+{
+    // Change F7FeatherV2 to F7FeatherV1 for V1.x boards
+    public class MeadowApp : App<F7CoreComputeV2>
+    {
+        //<!=SNIP=>
+
+        private IProjectLabHardware projectLab;
+        private C420R receiver;
+
+        public override Task Initialize()
+        {
+            Console.WriteLine("Initializing ...");
+
+            projectLab = ProjectLab.Create();
+
+            receiver = new C420R(projectLab.MikroBus1.SpiBus, projectLab.MikroBus1.Pins.CS);
+
+            return Task.CompletedTask;
+        }
+
+        public override async Task Run()
+        {
+            while (true)
+            {
+                var r = await receiver.Read();
+                Resolver.Log.Info($"Reading: {r.Milliamps:0.00} mA");
+                await Task.Delay(100);
+            }
+        }
+
+        private void OnCurrentUpdated(object sender, IChangeResult<Meadow.Units.Current> e)
+        {
+            Resolver.Log.Info($"Current changed from {(e.Old ?? new Meadow.Units.Current(0)).Amps}A to {e.New.Amps}A");
+        }
+
+        //<!=SNOP=>
+    }
+}
---
+++ Source/C420T/Sample/C420T_Sample/MeadowApp.cs
@@ -0,0 +1,62 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.mikroBUS.Sensors;
+using System;
+using System.Threading.Tasks;
+
+namespace CACCurrent_Sample
+{
+    // Change F7FeatherV2 to F7FeatherV1 for V1.x boards
+    public class MeadowApp : App<F7CoreComputeV2>
+    {
+        //<!=SNIP=>
+
+        private C420T transmitter;
+        private C420R receiver;
+
+        private IProjectLabHardware projectLab;
+
+        public override Task Initialize()
+        {
+            Console.WriteLine("Initializing ...");
+
+            projectLab = ProjectLab.Create();
+
+            transmitter = new C420T(projectLab.MikroBus2.SpiBus, projectLab.MikroBus2.Pins.CS);
+            receiver = new C420R(projectLab.MikroBus1.SpiBus, projectLab.MikroBus1.Pins.CS);
+
+            return Task.CompletedTask;
+        }
+
+        public override async Task Run()
+        {
+            var ma = 4;
+            var direction = 1;
+
+            while (true)
+            {
+                ma += direction;
+                if (ma == 20)
+                {
+                    direction = -1;
+                }
+                else if (ma == 4)
+                {
+                    direction = 1;
+                }
+
+                var val = new Meadow.Units.Current(ma, Meadow.Units.Current.UnitType.Milliamps);
+
+                Resolver.Log.Info($"Writing: {val.Milliamps:0.00} mA");
+                transmitter?.GenerateOutput(val);
+
+                var read = await receiver.Read();
+                Resolver.Log.Info($"Reading: {read.Milliamps:0.00} mA");
+
+                await Task.Delay(1000);
+            }
+        }
+
+        //<!=SNOP=>
+    }
+}
